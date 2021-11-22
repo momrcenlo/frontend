b2d9595836abd287550f5db76ea82dd23d323af4
momrcenlo <77125096+momrcenlo@users.noreply.github.com>
2021-11-22 17:08:22 +0100

    Update poruke.js

--- src/services/poruke.js
+++ src/services/poruke.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const osnovniUrl = 'http://localhost:3001/api/poruke'
+const osnovniUrl = 'https://poruke-backend-mol.herokuapp.com/'
  
 const dohvatiSve = () => {   
     return axios.get(osnovniUrl);
